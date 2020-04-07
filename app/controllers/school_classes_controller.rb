7855e6b2f5dda1200feee829e1fa3e8fee6e4ab0
Eva Navarro <emanao@me.com>
2020-04-07 01:34:49 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,7 +4,11 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+<<<<<<< HEAD
     sclass = SchoolClass.new(school_params(:title, :room_number))
+=======
+    sclass = SchoolClass.new(post_params(:title, :room_number))
+>>>>>>> 5651b8f17c872fa0d037723edb69d628b134a345
     sclass.save
     redirect_to school_class_path sclass
   end
@@ -19,12 +23,20 @@ class SchoolClassesController < ApplicationController
 
   def update
     sclass = SchoolClass.find(params[:id])
+<<<<<<< HEAD
     sclass.update(school_params(:title, :room_number))
+=======
+    sclass.update(post_params(:title, :room_number))
+>>>>>>> 5651b8f17c872fa0d037723edb69d628b134a345
     redirect_to school_class_path sclass
   end
 
   private
+<<<<<<< HEAD
   def school_params(*args)
+=======
+  def post_params(*args)
+>>>>>>> 5651b8f17c872fa0d037723edb69d628b134a345
     params.require(:school_class).permit(*args)
   end
 end
